09c4ad995e6932b6221e10ba789eeee4d6b0591d
Georgios Karamoussanlis <georgios.karamoussanlis@tu-dortmund.de>
2022-03-18 13:19:06 +0100

    login feld erstellt

--- my-app/my-app/src/App.tsx
+++ my-app/my-app/src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Form, Button, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [logInStatus , setLogIn] = useState(false);
+  const [userName , setUserName] = useState("");
+  const [password , setPassword] = useState("")
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <React.Fragment>
+    {!logInStatus &&
+      <Card body className='middle'>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="email" placeholder="Nutzername" onChange={(event)=> setUserName(event.target.value)}/>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" name="password" onChange={(event)=> setPassword(event.target.value)} />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={() => {if(password === "1" && userName === "1"){setLogIn(true)}}}>
+          Log In
+        </Button>
+      </Form>
+    </Card>
+    }
+    {logInStatus && 
+      <p>geschafft</p>
+    }
+    </React.Fragment>
   );
 }
 
